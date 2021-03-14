c4156a1cd194246b586e0db02f616e0ed18a6c14
Liuming <lfan.canada@gmail.com>
2021-03-14 22:10:46 +0000

    page fetcher and save info as HTML

---
+++ fetcher.js
@@ -0,0 +1,29 @@
+
+const request = require('request');
+const fs = require('fs');
+
+const URL = process.argv[2];
+const path = process.argv[3];
+
+
+request(URL, function(error, response, body) {
+  console.error('error:', error);
+  console.log('statusCode:', response && response.statusCode);
+
+  if (error) {
+    console.log('error from request');
+    return;
+  }
+
+  if (response && response.statusCode !== 200) {
+    console.log('bad statusCode');
+    return;
+  }
+
+  fs.writeFile(path, body, (err) => {
+    if (err) throw err;
+    console.log(`Downloaded and saved ${body.length} bytes to ${path}`);
+  });
+
+});
+
